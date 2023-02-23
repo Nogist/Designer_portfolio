619bacfd4a0aa2bfbf153585a190251e8e25a2fc
Nogist <nogistune.me@gmail.com>
2023-02-23 13:31:04 -0800

    upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import Logo from '../assets/Vector.png';
+import styles from '@/styles/Home.module.css';
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex items-center justify-between py-[15px] px-[150px] bg-[#66000000]" id='blo'>
-      <div>
-        <Image 
-          src={Logo} alt="logo"
-          width={102.87}
-          height={69.41}
-         />
-      </div>
-      <div>
-        <p className="text-white">Doc</p>
+    <nav className=' absolute  top-0 left- 0 w-full '>
+      <div className="flex justify-between px-[150px] pt-4 .big-text">
+        <div className="nav-logo">
+          <Image 
+            src={Logo} 
+            alt="logo"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <p className="text-3xl font-bold leading-10 big-text">Docs</p>
       </div>
     </nav>
   )
